4e356d3fcea2780ad62691166e43bd0180258821
Agueda Gomez Cambronero <al285895@uji.es>
2016-03-28 22:15:34 +0200

    Script cameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    Vector3 offset;                     // The initial offset from the target.
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * smoothing);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
