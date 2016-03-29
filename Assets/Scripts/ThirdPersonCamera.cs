c3497abba499a3f66de8214993606e46968f8fab
Agueda Gomez Cambronero <al285895@uji.es>
2016-03-29 21:05:58 +0200

    Terminado script de la cámara

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public Transform target;
+    float distance = 10.0f;
+    float height = 3.0f;
+    float damping = 5.0f;
+    bool smoothRotation = true;
+    float rotationDamping = 10.0f;
+
+    void FixedUpdate()
+    {
+        Vector3 wantedPosition = target.TransformPoint(0, height, distance);
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+
+        if (smoothRotation)
+        {
+            Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+        }
+
+        else transform.LookAt(target, target.up);
+    }
+}
