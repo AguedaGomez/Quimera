66ebeef87cf58d7194aab8bfc60e66bfd75787d7
Ignacio López Rivas <al285779@uji.es>
2016-03-29 22:16:58 +0200

    Eliminamos lo planetas, y preparamos la entrega

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,8 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+    public float m_Speed = 30f;                 // How fast the tank moves forward and back.
     public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
-    //public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-    //public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-    //public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-    //public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
 
 
     private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
@@ -27,7 +23,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private void OnEnable()
     {
-        // When the tank is turned on, make sure it's not kinematic.
+        // When the ship is turned on, make sure it's not kinematic.
         m_Rigidbody.isKinematic = false;
 
         // Also reset the input values.
@@ -38,7 +34,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private void OnDisable()
     {
-        // When the tank is turned off, set it to kinematic so it stops moving.
+        // When the ship is turned off, set it to kinematic so it stops moving.
         m_Rigidbody.isKinematic = true;
     }
 
@@ -48,9 +44,6 @@ public class PlayerMovement : MonoBehaviour {
         // The axes names are based on player number.
         m_MovementAxisName = "Vertical";
         m_TurnAxisName = "Horizontal";
-
-        // Store the original pitch of the audio source.
-       // m_OriginalPitch = m_MovementAudio.pitch;
     }
 
 
@@ -59,37 +52,8 @@ public class PlayerMovement : MonoBehaviour {
         // Store the value of both input axes.
         m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
         m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
-
-       // EngineAudio();
+        Debug.Log("TEST: turninputvalue = " + m_TurnInputValue);
     }
-
-
-   /* private void EngineAudio()
-    {
-        // If there is no input (the tank is stationary)...
-        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
-        {
-            // ... and if the audio source is currently playing the driving clip...
-            if (m_MovementAudio.clip == m_EngineDriving)
-            {
-                // ... change the clip to idling and play it.
-                m_MovementAudio.clip = m_EngineIdling;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play();
-            }
-        }
-        else
-        {
-            // Otherwise if the tank is moving and if the idling clip is currently playing...
-            if (m_MovementAudio.clip == m_EngineIdling)
-            {
-                // ... change the clip to driving and play.
-                m_MovementAudio.clip = m_EngineDriving;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play();
-            }
-        }
-    }*/
 
 
     private void FixedUpdate()
@@ -98,7 +62,6 @@ public class PlayerMovement : MonoBehaviour {
         Move();
         Turn();
     }
-
 
     private void Move()
     {
@@ -115,9 +78,6 @@ public class PlayerMovement : MonoBehaviour {
         // Determine the number of degrees to be turned based on the input, speed and time between frames.
         float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
-        Debug.Log("TEST: turninputvalue = " + m_TurnInputValue);
-
-        transform.Rotate(0, 0, m_TurnInputValue);
         // Make this into a rotation in the y axis.
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
 
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -3,24 +3,24 @@ using System.Collections;
 
 public class ThirdPersonCamera : MonoBehaviour {
 
-    public Transform target;
-    float distance = 10.0f;
-    float height = 3.0f;
-    float damping = 5.0f;
+    public Transform target; // el jugador
+    float distance = 10.0f; // distancia entre la camara y el jugador
+    float height = 3.0f; // altura de la camara
+    float damping = 5.0f; // Suavizado del movimiento de camara en movimientos rectos
     bool smoothRotation = true;
-    float rotationDamping = 10.0f;
+    float rotationDamping = 10.0f; // Suavizado del movimiento de camara en movimientos curvos
 
     void FixedUpdate()
     {
         Vector3 wantedPosition = target.TransformPoint(0, height, distance);
         transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
 
-        if (smoothRotation)
-        {
+        //if (smoothRotation)
+        //{
             Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
             transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
-        }
+        //}
 
-        else transform.LookAt(target, target.up);
+        //else transform.LookAt(target, target.up);
     }
 }
