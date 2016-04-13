d9f093689866c801db590d460cab7de3babb8d61
Ignacio López Rivas <al285779@uji.es>
2016-04-13 18:21:25 +0200

    Incluye los planetas y un script de rotacion para los planetas

---
+++ Assets/RotationPlanet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RotationPlanet : MonoBehaviour {
+
+    private Rigidbody planetRigidBody;
+
+    private void Awake()
+    {
+        planetRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        rotatePlanet();
+    }
+
+    private void rotatePlanet()
+    {
+        float rotationAxis = 0.15f * Time.frameCount;
+        Quaternion rotationAngle = Quaternion.Euler(rotationAxis, 0f, rotationAxis);
+
+        planetRigidBody.MoveRotation(rotationAngle );
+    }
+}
